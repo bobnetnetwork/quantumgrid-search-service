02410450c71ad84db7d1d4688bc6e926e39028bf
smaso_JaszayN <bob@bobnet.network>
2024-09-02 21:38:44 +0200

    QUAN-52 Set up project structure using Spring Boot

---
+++ src/main/java/network/bobnet/quantumgrid/search_service/SearchServiceApplication.java
@@ -0,0 +1,16 @@
+package network.bobnet.quantumgrid.search_service;
+
+import lombok.Generated;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Generated
+@SpringBootApplication
+public class SearchServiceApplication {
+
+	@Generated
+	public static void main(String[] args) {
+		SpringApplication.run(SearchServiceApplication.class, args);
+	}
+
+}
